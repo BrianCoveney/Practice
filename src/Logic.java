81be0a415e09d219c07d1e0f8db9d5d84ba003b7
Brian Coveney <coveneybrian@gmail.com>
2017-07-24 12:12:14 +0100

    Add sumLimit()

--- src/Logic.java
+++ src/Logic.java
@@ -122,4 +122,16 @@ public class Logic {
     }
 
 
+    // If the sum has more digits than a, just return a without b. Else, return sum
+    public int sumLimit(int a, int b) {
+        int sum = a+b;
+
+        if(String.valueOf(sum).length() > String.valueOf(a).length()) {
+            return a;
+        }
+
+        return sum;
+    }
+
+
 }
